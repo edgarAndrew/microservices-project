130cd8b7d8df9d98c8ea322ea5c9388f9f353b60
edgarAndrew <edgarcamelo03@gmail.com>
2024-09-11 01:21:25 +0530

    made dev and prod spring profiles along with sql dump file for each service

--- quiz_service/data.sql
+++ quiz_service/data.sql
@@ -14,6 +14,156 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `HTE_quizzes`
+--
+
+DROP TABLE IF EXISTS `HTE_quizzes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `HTE_quizzes` (
+  `id` varchar(0) DEFAULT NULL,
+  `rn_` varchar(0) DEFAULT NULL,
+  `created_at` varchar(0) DEFAULT NULL,
+  `updated_at` varchar(0) DEFAULT NULL,
+  `hib_sess_id` varchar(0) DEFAULT NULL,
+  `description` varchar(0) DEFAULT NULL,
+  `quiz_date` varchar(0) DEFAULT NULL,
+  `title` varchar(0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `HTE_quizzes`
+--
+
+LOCK TABLES `HTE_quizzes` WRITE;
+/*!40000 ALTER TABLE `HTE_quizzes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `HTE_quizzes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Persons`
+--
+
+DROP TABLE IF EXISTS `Persons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Persons` (
+  `PersonID` tinyint(4) DEFAULT NULL,
+  `LastName` varchar(7) DEFAULT NULL,
+  `FirstName` varchar(6) DEFAULT NULL,
+  `Address` varchar(11) DEFAULT NULL,
+  `City` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Persons`
+--
+
+LOCK TABLES `Persons` WRITE;
+/*!40000 ALTER TABLE `Persons` DISABLE KEYS */;
+INSERT INTO `Persons` VALUES (1,'Smith','John','123 Main St','New York'),(2,'Johnson','Alice','456 Elm St','Los Angeles'),(3,'Brown','Robert','789 Oak Ave','Chicago');
+/*!40000 ALTER TABLE `Persons` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quiz_questions`
+--
+
+DROP TABLE IF EXISTS `quiz_questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quiz_questions` (
+  `question_id` tinyint(4) DEFAULT NULL,
+  `quiz_id` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quiz_questions`
+--
+
+LOCK TABLES `quiz_questions` WRITE;
+/*!40000 ALTER TABLE `quiz_questions` DISABLE KEYS */;
+INSERT INTO `quiz_questions` VALUES (1,1),(2,1),(3,1),(4,1),(5,1);
+/*!40000 ALTER TABLE `quiz_questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quiz_registration`
+--
+
+DROP TABLE IF EXISTS `quiz_registration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quiz_registration` (
+  `quiz_id` smallint(6) DEFAULT NULL,
+  `user_id` tinyint(4) DEFAULT NULL,
+  `reg_date` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quiz_registration`
+--
+
+LOCK TABLES `quiz_registration` WRITE;
+/*!40000 ALTER TABLE `quiz_registration` DISABLE KEYS */;
+INSERT INTO `quiz_registration` VALUES (152,1,'16-08-2024'),(152,4,'10-09-2024');
+/*!40000 ALTER TABLE `quiz_registration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quizzes`
+--
+
+DROP TABLE IF EXISTS `quizzes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quizzes` (
+  `id` smallint(6) DEFAULT NULL,
+  `created_at` bigint(20) DEFAULT NULL,
+  `description` varchar(23) DEFAULT NULL,
+  `quiz_date` varchar(10) DEFAULT NULL,
+  `title` varchar(14) DEFAULT NULL,
+  `updated_at` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quizzes`
+--
+
+LOCK TABLES `quizzes` WRITE;
+/*!40000 ALTER TABLE `quizzes` DISABLE KEYS */;
+INSERT INTO `quizzes` VALUES (1,1760624403,'This is a sample quiz 3','10-07-2024','Sample Quiz 3',-664217480),(102,-1419312319,'This is a sample quiz','10-07-2024','Sample Quiz 9',-1419312319),(152,1450565525,'This is a sample quiz','15-10-2024','Sample Quiz 19',1450565525);
+/*!40000 ALTER TABLE `quizzes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quizzes_seq`
+--
+
+DROP TABLE IF EXISTS `quizzes_seq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quizzes_seq` (
+  `next_val` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quizzes_seq`
+--
+
+LOCK TABLES `quizzes_seq` WRITE;
+/*!40000 ALTER TABLE `quizzes_seq` DISABLE KEYS */;
+INSERT INTO `quizzes_seq` VALUES (251);
+/*!40000 ALTER TABLE `quizzes_seq` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +174,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-30 16:42:38
+-- Dump completed on 2024-04-30 16:42:47
